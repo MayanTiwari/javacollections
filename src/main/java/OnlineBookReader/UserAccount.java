de39e8e1ffa87c34a2b5cfcbb5be5015bebe5d6b
mayantiwari <mayantiwari@hotmail.com>
2017-09-09 22:31:28 +0530

    Added new file for user activities

--- src/main/java/OnlineBookReader/UserAccount.java
+++ src/main/java/OnlineBookReader/UserAccount.java
@@ -4,7 +4,7 @@ package OnlineBookReader;
  * Created by mayan on 9/9/17.
  */
 public interface UserAccount {
-    User registerNewUser();
+    void registerNewUser();
     void updateUser(User user);
     void removeUser(User user);
 }
