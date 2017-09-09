1b8d953d4383f64f6c7e5f7cd284557e21061c30
mayantiwari <mayantiwari@hotmail.com>
2017-09-09 22:32:31 +0530

    Added new file for user activities

--- src/main/java/OnlineBookReader/UserManager.java
+++ src/main/java/OnlineBookReader/UserManager.java
@@ -1,7 +1,25 @@
 package OnlineBookReader;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mayan on 9/9/17.
  */
-public class UserManager {
+public class UserManager implements UserAccount{
+    Map<Integer,User> userMap = new HashMap<>();
+    @Override
+    public void registerNewUser() {
+        userMap.put(1,new User());
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public void removeUser(User user) {
+
+    }
 }
